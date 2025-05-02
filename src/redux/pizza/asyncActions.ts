3014cd3616d00a8c339c1b1c70c98ff97cc38ab0
David Jelezoglo <davidoskin007@gmail.com>
2025-05-02 15:40:46 +0300

    Finish project

---
+++ src/redux/pizza/asyncActions.ts
@@ -0,0 +1,28 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { Pizza, SearchPizzaParams } from './types';
+import pickBy from 'lodash/pickBy';
+import identity from 'lodash/identity';
+
+export const fetchPizzas = createAsyncThunk<Pizza[], SearchPizzaParams>(
+  'pizza/fetchPizzasStatus',
+  async (params) => {
+    const { sortBy, order, category, search, currentPage } = params;
+    console.log(params, 4444);
+    const { data } = await axios.get<Pizza[]>(`https://678d5804f067bf9e24ea0f3c.mockapi.io/items`, {
+      params: pickBy(
+        {
+          page: currentPage,
+          limit: 8,
+          category,
+          sortBy,
+          order,
+          search,
+        },
+        identity,
+      ),
+    });
+
+    return data;
+  },
+);
